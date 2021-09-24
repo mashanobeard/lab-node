d6e13e4e0cad385da78bebcf8437d32d74c42dee
mashanobeard <maryk016061@gmail.com>
2021-09-24 16:01:53 +0300

    init-com

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-import express  from "express"
 
-const PORT = 4000;
+
+import express from "express"
+
+const PORT = process.env.PORT || 4000;
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.end(
         '<h1><a href="/api/greetings">Your name</a></h1>'
     )
  })
-app.get('/api', (req,res) => {
-   res.end(
-       '<h1>Home page</h1>'
-   )
-})
 app.get('/api/greetings', (req,res) => {
-    res.send('<h1>Hellooo</h1> + ${name}')
+   res.send('<h3> Hellooo  ' + req.query.name + "</h3>")
+//    if(!res.send){
+//      res.status(400).json( "incorrect")  
+//     }  
  })
 app.listen(PORT, () =>{
-   console.log ('SERVER STARTED ON PORT' + PORT)
+   console.log ('SERVER STARTED ON PORT ' + PORT)
 })
+
+
