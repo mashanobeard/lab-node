0b1f0890e2d0425ba607f7ee8a7cb54fb49d673f
mashanobeard <maryk016061@gmail.com>
2021-11-03 12:53:59 +0300

    swagger

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import router from './modules/notes/routes/index.js';
 import router1 from './modules/greeting/routes/index.js';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+//import * as swaggerDocument from './swagger/openapi.json';
+
 import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -15,6 +19,24 @@ app.use('/api/notes', router);
 app.use('/api/greetings', router1);
 
 app.use(morgan(process.env.LOG_LEVEL)); //logger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Test API',
+      version: '1.0.0',
+      description: 'My notes project API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000/',
+      },
+    ],
+  },
+  apis: ['./modules/notes/routes/*.js'],
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 async function startApp() {
   try {
--- modules/notes/routes/index.js
+++ modules/notes/routes/index.js
@@ -9,14 +9,144 @@ import updateSchema from '../validation/updateSchema.js';
 const router = express.Router();
 
 //read
+/**
+ *  @swagger
+ * components:
+ *   schemas:
+ *     Note:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id
+ *         title:
+ *           type: string
+ *           minLength: 3
+ *         content:
+ *           type: string
+ *           minLength: 3
+ *           maxLength: 500
+ *         isDeleted:
+ *           type: boolean
+ *           default: false
+ *       example:
+ *         id: 616804ccd5464b06e00a5856
+ *         title: cat
+ *         content: meow
+ *         createdAt: 2021-10-14T10:20:44.537Z
+ *         updatedAt: 2021-10-14T10:20:54.537Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Notes
+ *   description: API
+ */
+
+/**
+ * @swagger
+ * /api/notes:
+ *  get:
+ *      summary: Get notes
+ *      tags: [Notes]
+ *      responses:
+ *         "200":
+ *          description: A successful response
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                           $ref: '#/components/schemas/Note'
+ */
 router.get('/', getController);
 
 //create //validation
+
+/**
+ * @swagger
+ * /api/notes:
+ *  post:
+ *      summary: Create note
+ *      tags: [Notes]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Note'
+ *      responses:
+ *        "200":
+ *           description: A successful response
+ *           content:
+ *                application/json:
+ *                    schema:
+ *                       $ref: '#/components/schemas/Note'
+ *        "500":
+ *           description: some server error
+ */
 router.post('/', createSchema, createController);
 //update //validation
+/**
+ * @swagger
+ * /api/notes/{id}:
+ *  put:
+ *      summary: Update note
+ *      tags: [Notes]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: Id of the note you want to update
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Note'
+ *      responses:
+ *        "200":
+ *           description: The note was updated
+ *           content:
+ *                application/json:
+ *                    schema:
+ *                       $ref: '#/components/schemas/Note'
+ *        "404":
+ *           description: The note wasn't found
+ *        "500":
+ *           description: some server error
+ */
 router.put('/:id', updateSchema, updateController);
 
 //delete
+/**
+ * @swagger
+ * /api/notes/{id}:
+ *  delete:
+ *      summary: Delete note
+ *      tags: [Notes]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: Id of the note you want to delete
+ *      responses:
+ *        "200":
+ *           description: The note was deleted
+ *           content:
+ *                application/json:
+ *                    schema:
+ *                       $ref: '#/components/schemas/Note'
+ *        "404":
+ *           description: The note wasn't found
+ *        "500":
+ *           description: some server error
+ */
 router.delete('/:id', deleteController);
 
 export default router;
